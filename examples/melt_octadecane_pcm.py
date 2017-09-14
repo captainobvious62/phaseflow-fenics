242bf65c4d323c3157b7fd2c0dcd84914e01284c
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-09-14 21:09:36 +0200

    In work: This doesn't seem to be going well at all

--- examples/melt_octadecane_pcm.py
+++ examples/melt_octadecane_pcm.py
@@ -1,24 +1,5 @@
 import fenics
 import phaseflow
-import scipy.optimize as opt
-        
-def verify_pci_position_regression(theta_s, w):
-
-    ref_data = {'Ra': 1., 'Pr': 1., 'theta_s': 0.1, 'R_s': 0.05, 'Ste': 1., 'mu_s': 1.e4,
-            'nlp_absolute_tolerance': 1., 'time': 0.001, 'pci_y_pos': 0.5, 'pci_x_pos': 0.043}
-
-    assert(theta_s == ref_data['theta_s'])
-            
-    def theta_minus_theta_s(x):
-    
-        wval = w(fenics.Point(x, ref_data['pci_y_pos']))
-        
-        return wval[3] - theta_s
-    
-    pci_x_pos = opt.newton(theta_minus_theta_s, 0.01)
-    
-    assert(abs(pci_x_pos - ref_data['pci_x_pos']) < 1.e-3)
-        
         
 def melt_pcm(Ste = 0.045,
         Ra = 3.27e5,
@@ -26,16 +7,16 @@ def melt_pcm(Ste = 0.045,
         mu_s = 1.e8,
         theta_s = 0.01,
         R_s = 0.005,
-        m = 10,
+        m = 100,
         time_step_bounds = (1.e-4, 1.e-3, 1.e-3),
-        initial_pci_refinement_cycles = 2,
+        initial_pci_refinement_cycles = 0,
         max_pci_refinement_cycles_per_time = 6,
         output_dir='output/melt_pcm',
         start_time=0.,
         end_time=78.7,
         nlp_absolute_tolerance = 1.,
         nlp_divergence_threshold = 1.e12,
-        nlp_max_iterations = 30,
+        nlp_max_iterations = 50,
         restart=False,
         restart_filepath=''):
 
@@ -86,35 +67,12 @@ def melt_pcm(Ste = 0.045,
     return w, mesh
 
 
-def test_melt_octadecane_pcm():
+def run_melt_octadecane_pcm():
 
     melt_pcm(output_dir='output/melt_octadecane_pcm')
     
     
     
-def test_melt_pcm():
-
-    theta_s = 0.1
-    
-    w, mesh = melt_pcm(Ste = 1.,
-        Ra = 1.,
-        Pr = 1.,
-        mu_s = 1.e4,
-        theta_s = theta_s,
-        R_s = theta_s/2.,
-        m = 1,
-        time_step_bounds = (1.e-4, 1.e-4, 1.e-3),
-        end_time = 1.e-3,
-        initial_pci_refinement_cycles = 5,
-        max_pci_refinement_cycles_per_time = 2,
-        nlp_divergence_threshold = 1.e12,
-        output_dir = 'output/test_melt_pcm')
-        
-    verify_pci_position_regression(theta_s, w)
-    
-    
 if __name__=='__main__':
 
-    test_melt_pcm()
-    
-    test_melt_octadecane_pcm()
+    run_melt_octadecane_pcm()
