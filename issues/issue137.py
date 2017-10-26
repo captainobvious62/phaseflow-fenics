24ee3e4909679039d5dd3ccc8712221f433f4e1a
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-10-26 16:42:07 +0200

    In work: Debugging adaptive solver with 1d stefan problem

--- issues/issue137.py
+++ issues/issue137.py
@@ -4,32 +4,6 @@ where adaptivenonlinearvariationalsolver produces NaN's
 import fenics
 
 
-pressure_degree = 1
-
-temperature_degree = 1
-
-def make_mixed_fe(mesh):
-    """ Define mixed FE function space for the variational form."""
-    
-    velocity_degree = pressure_degree + 1
-
-    ''' MixedFunctionSpace used to be available but is now deprecated. 
-    The way that fenics separates function spaces and elements is confusing.
-    To create the mixed space, I'm using the approach from https://fenicsproject.org/qa/11983/mixedfunctionspace-in-2016-2-0
-    '''
-    velocity_element = fenics.VectorElement('P', mesh.ufl_cell(), velocity_degree)
-
-    pressure_element = fenics.FiniteElement('P', mesh.ufl_cell(), pressure_degree)
-
-    temperature_element = fenics.FiniteElement('P', mesh.ufl_cell(), temperature_degree)
-
-    solution_element = fenics.MixedElement([velocity_element, pressure_element, temperature_element])
-
-    solution_function_space = fenics.FunctionSpace(mesh, solution_element)  
-    
-    return solution_function_space, solution_element
-
-    
 def write_solution(solution_file, w, time):
     """Write the solution to disk."""
     print("Writing solution to HDF5+XDMF")
@@ -49,17 +23,33 @@ def write_solution(solution_file, w, time):
         
 class Model():
 
-
-    def __init__(self, W, initial_values_expression, bcs, time, time_step_size,
-            liquid_viscosity, rayleigh_number, prandtl_number, gravity, temperature_of_fusion):
+    def __init__(self, mesh, pressure_degree, temperature_degree,
+            initial_values, boundary_conditions, time, time_step_size,
+            liquid_viscosity, rayleigh_number, prandtl_number, stefan_number,
+            gravity, temperature_of_fusion, smoothing_radius):
     
-        self.W = W
+        self.mesh = mesh
         
-        self.w_k = fenics.Function(W)
+        self.pressure_degree = pressure_degree
         
-        self.w_n = fenics.interpolate(initial_values_expression, W)
+        self.temperature_degree = temperature_degree
         
-        self.bcs = bcs
+        self.velocity_degree = self.pressure_degree + 1
+        
+        self.W, self.W_ele = self.make_mixed_fe(mesh)
+        
+        self.w_k = fenics.Function(self.W)
+        
+        self.w_n = fenics.interpolate(
+            fenics.Expression(initial_values, element=self.W_ele),
+            self.W)
+        
+        self.bcs = []
+    
+        for item in boundary_conditions:
+
+            self.bcs.append(fenics.DirichletBC(self.W.sub(item['subspace']),
+                item['value'], item['location'], method=item['method']))
         
         self.time = time
         
@@ -75,22 +65,46 @@ class Model():
         
         self.prandtl_number = prandtl_number
         
+        self.stefan_number = stefan_number
+        
         self.gravity = gravity
 
         self.heat_capacity = 1.
 
         self.thermal_conductivity = 1.
 
-        self.stefan_number = 1.
-
         self.penalty_factor = 1.e-7
 
         self.temperature_of_fusion = temperature_of_fusion
         
-        self.smoothing_radius = 0.05
+        self.smoothing_radius = smoothing_radius
         
         
-    def solve(self, adaptive):
+    def make_mixed_fe(self, mesh):
+        """ Define mixed FE function space for the variational form."""
+        
+        ''' MixedFunctionSpace used to be available but is now deprecated. 
+        The way that fenics separates function spaces and elements is confusing.
+        To create the mixed space, I'm using the approach from https://fenicsproject.org/qa/11983/mixedfunctionspace-in-2016-2-0
+        '''
+        velocity_element = fenics.VectorElement('P', mesh.ufl_cell(),
+            self.velocity_degree)
+
+        pressure_element = fenics.FiniteElement('P', mesh.ufl_cell(),
+            self.pressure_degree)
+
+        temperature_element = fenics.FiniteElement('P', mesh.ufl_cell(),
+            self.temperature_degree)
+
+        solution_element = fenics.MixedElement([velocity_element, pressure_element, 
+            temperature_element])
+
+        solution_function_space = fenics.FunctionSpace(mesh, solution_element)  
+        
+        return solution_function_space, solution_element
+        
+        
+    def solve(self, adaptive, adaptive_tolerance=None):
         
         mu_l = fenics.Constant(self.liquid_viscosity)
         
@@ -113,10 +127,10 @@ class Model():
         Ste = fenics.Constant(self.stefan_number)
 
         gamma = fenics.Constant(self.penalty_factor)
-
-        r = fenics.Constant(self.smoothing_radius)
         
         T_f = fenics.Constant(self.temperature_of_fusion)
+        
+        r = fenics.Constant(self.smoothing_radius)
         
         inner, dot, grad, div = fenics.inner, fenics.dot, fenics.grad, fenics.div
         
@@ -190,11 +204,11 @@ class Model():
         # Solve the problem.
         if adaptive:
 
-            M = (u_k[0] + u_k[1] + T_k + P(T_k))*fenics.dx  # Adaptive goal functional
+            M = (u_k[0] + T_k + P(T_k))*fenics.dx  # Adaptive goal functional
     
             solver = fenics.AdaptiveNonlinearVariationalSolver(problem, M)
             
-            solver.solve(1.e-4)
+            solver.solve(adaptive_tolerance)
         
         else:
         
@@ -204,53 +218,58 @@ class Model():
             
         self.time += self.time_step_size
         
+        
         # Update initial values for the next time step.
         self.w_n.leaf_node().vector()[:] = self.w_k.leaf_node().vector()  
     
 
 def test_adaptive_natural_convection_in_differentially_heated_cavity():
     
-    m = 20
+    m = 10
     
     mesh = fenics.UnitSquareMesh(m, m, 'crossed')
     
-    W, W_ele = make_mixed_fe(mesh)
-    
     T_hot, T_cold = 0.5, -0.5
-    
-    
-    # Make the model.
+
     start_time, end_time = 0., 10.
     
     time_step_size = 1.e-3
     
-    model = Model(W=W,
-        initial_values_expression=fenics.Expression(("0.", "0.", "0.",
-            str(T_hot)+"*near(x[0],  0.) "+str(T_cold)+"*near(x[0],  1.)"), element=W_ele),
-        bcs=[
-            fenics.DirichletBC(W.sub(0), ("0.", "0."), 
-                "near(x[0],  0.) | near(x[0],  1.) | near(x[1], 0.) | near(x[1],  1.)",
-                method='topological'),
-            fenics.DirichletBC(W.sub(2), str(T_hot), "near(x[0],  0.)", method='topological'),
-            fenics.DirichletBC(W.sub(2), str(T_cold), "near(x[0],  1.)", method='topological')],
+    model = Model(mesh = mesh,
+        pressure_degree = 1,
+        temperature_degree = 1,
+        initial_values = ("0.", "0.", "0.",
+            str(T_hot)+"*near(x[0],  0.) "+str(T_cold)+"*near(x[0],  1.)"),
+        boundary_conditions = [
+            {'subspace': 0, 'value': ("0.", "0."),
+            'location': "near(x[0],  0.) | near(x[0],  1.) | near(x[1], 0.) | near(x[1],  1.)",
+            'method': 'topological'},
+            {'subspace': 2, 'value': str(T_hot),
+            'location': "near(x[0],  0.)",
+            'method': 'topological'},
+            {'subspace': 2, 'value': str(T_cold),
+            'location': "near(x[0],  1.)",
+            'method': 'topological'}],
         time=start_time, time_step_size=time_step_size, liquid_viscosity=1.,
-        rayleigh_number=1.e6, prandtl_number=0.71, gravity=(0., -1.),
-        temperature_of_fusion=-1.)
+        rayleigh_number=1.e6, prandtl_number=0.71, stefan_number = 1.,
+        gravity=(0., -1.),
+        temperature_of_fusion=-1., smoothing_radius=0.05)
     
-    
-    # Solve a sequence of time steps.
     progress = fenics.Progress('Time-stepping')
 
     fenics.set_log_level(fenics.PROGRESS)
         
     with fenics.XDMFFile('output/natural_convection/solution.xdmf') as solution_file:
     
+    
         # Write the initial values
         write_solution(solution_file, model.w_n, model.time)
             
+            
+        # Solve a sequence of time steps.
         while model.time < (end_time - fenics.DOLFIN_EPS):
         
-            model.solve(adaptive=True)
+            model.solve(adaptive=True, adaptive_tolerance=1.e-8)
 
             write_solution(solution_file, model.w_k, model.time)
             
@@ -269,27 +288,27 @@ def lid_driven_cavity(adaptive):
 
     bottom_left_corner = 'near(x[0], 0.) && near(x[1], 0.)'
     
-    m = 10
+    m = 3
     
     mesh = fenics.UnitSquareMesh(m, m, 'crossed')
-    
-    W, W_ele = make_mixed_fe(mesh)
-    
-    
-    # Make the problem.    
-    model = Model(W=W,
-        initial_values_expression=fenics.Expression((lid, "0.", "0.", "0."), element=W_ele), 
-        bcs=[
-            fenics.DirichletBC(W.sub(0), ("1.", "0."), lid,  method='topological'),
-            fenics.DirichletBC(W.sub(0), ("0.", "0."), fixed_walls, method='topological'),
-            fenics.DirichletBC(W.sub(1), "0.", bottom_left_corner, method='pointwise')],
+  
+    model = Model(mesh=mesh,
+        pressure_degree = 1,
+        temperature_degree = 1,
+        initial_values = (lid, "0.", "0.", "0."), 
+        boundary_conditions = [
+            {'subspace': 0, 'value': ("1.", "0."), 'location': lid,
+                'method': 'topological'},
+            {'subspace': 0, 'value': ("0.", "0."), 'location': fixed_walls,
+                'method': 'topological'},
+            {'subspace': 1, 'value': "0.", 'location': bottom_left_corner,
+                'method': 'pointwise'}],
         time=0., time_step_size=1.e12, liquid_viscosity=0.01,
-        rayleigh_number=1., prandtl_number=1., gravity=(0., 0.),
-        temperature_of_fusion=-1.)
-    
-    
-    # Solve the problem.
-    model.solve(adaptive=adaptive)
+        rayleigh_number=1., prandtl_number=1., stefan_number=1.,
+        gravity=(0., 0.),
+        temperature_of_fusion=-1., smoothing_radius = 0.05)
+
+    model.solve(adaptive=adaptive, adaptive_tolerance=1.e-8)
         
     with fenics.XDMFFile('output/lid_driven_cavity/solution.xdmf') as solution_file:
     
@@ -339,24 +358,29 @@ def test_adaptive_melting_pcm():
     
     
     # Make the model.
-    W, W_ele = make_mixed_fe(mesh)
-    
     start_time, end_time = 0., 0.002
     
     time_step_size = 1.e-3
     
-    model = Model(W=W,
-        initial_values_expression=fenics.Expression(("0.", "0.", "0.",
-            "("+str(T_hot)+" - "+str(T_cold)+")*(x[0] < 0.001) + "+str(T_cold)), element=W_ele),
-        bcs=[
-            fenics.DirichletBC(W.sub(0), ("0.", "0."), 
-                "near(x[0],  0.) | near(x[0],  1.) | near(x[1], 0.) | near(x[1],  1.)",
-                method='topological'),
-            fenics.DirichletBC(W.sub(2), str(T_hot), "near(x[0],  0.)", method='topological'),
-            fenics.DirichletBC(W.sub(2), str(T_cold), "near(x[0],  1.)", method='topological')],
+    model = Model(mesh=mesh,
+        pressure_degree = 1,
+        temperature_degree = 1,
+        initial_values=("0.", "0.", "0.",
+            "("+str(T_hot)+" - "+str(T_cold)+")*(x[0] < 0.001) + "+str(T_cold)),
+        boundary_conditions = [
+            {'subspace': 0, 'value': ("0.", "0."),
+            'location': "near(x[0],  0.) | near(x[0],  1.) | near(x[1], 0.) | near(x[1],  1.)",
+            'method': 'topological'},
+            {'subspace': 2, 'value': str(T_hot),
+            'location': "near(x[0],  0.)",
+            'method': 'topological'},
+            {'subspace': 2, 'value': str(T_cold),
+            'location': "near(x[0],  1.)",
+            'method': 'topological'}],
         time=start_time, time_step_size=time_step_size, liquid_viscosity=1.,
-        rayleigh_number=1., prandtl_number=1., gravity=(0., -1.),
-        temperature_of_fusion=0.1)
+        rayleigh_number=1., prandtl_number=1., stefan_number=1.,
+        gravity=(0., -1.),
+        temperature_of_fusion=0.1, smoothing_radius=0.05)
     
     
     # Solve a sequence of time steps.
@@ -371,7 +395,88 @@ def test_adaptive_melting_pcm():
             
         while model.time < (end_time - fenics.DOLFIN_EPS):
         
-            model.solve(adaptive=True)
+            model.solve(adaptive=True, adaptive_tolerance=1.e-4)
+
+            write_solution(solution_file, model.w_k, model.time)
+            
+            progress.update(model.time / end_time)
+            
+            
+def test_1d_stefan_problem():
+
+    mesh = fenics.UnitIntervalMesh(10)
+    
+    T_hot, T_cold = 1., -1
+    
+    
+    # Refine the initial mesh near the hot wall
+    initial_hot_wall_refinement_cycles = 10
+    
+    ''' Refine mesh near hot boundary
+    The usual approach of using SubDomain and EdgeFunction isn't appearing to work
+    in 1D, so I'm going to just loop through the cells of the mesh and set markers manually.
+    '''
+    for i in range(initial_hot_wall_refinement_cycles):
+    
+        cell_markers = fenics.CellFunction("bool", mesh)
+        
+        cell_markers.set_all(False)
+        
+        for cell in fenics.cells(mesh):
+        
+            found_hot_boundary = False
+        
+            for vertex in fenics.vertices(cell):
+            
+                if fenics.near(vertex.x(0), 0., fenics.dolfin.DOLFIN_EPS):
+                
+                    found_hot_boundary = True
+                    
+            if found_hot_boundary:
+            
+                cell_markers[cell] = True
+
+                break # There should only be one such point.
+                
+        mesh = fenics.refine(mesh, cell_markers)
+    
+    
+    # Make the model.
+    model = Model(mesh=mesh,
+        pressure_degree = 1,
+        temperature_degree = 1,
+        initial_values=("0.", "0.",
+            "("+str(T_hot)+" - "+str(T_cold)+")*near(x[0], 0.) + "+str(T_cold)),
+        boundary_conditions = [
+            {'subspace': 0, 'value': [0.],
+            'location': "near(x[0],  0.) | near(x[0],  1.)", 'method': 'topological'},
+            {'subspace': 2, 'value': T_hot,
+            'location': "near(x[0],  0.)", 'method': 'topological'},
+            {'subspace': 2, 'value': T_cold,
+            'location': "near(x[0],  1.)", 'method': 'topological'}],
+        time=0., time_step_size=1.e-4, liquid_viscosity=1.,
+        rayleigh_number=1., prandtl_number=1., stefan_number=0.1,
+        gravity=[0.],
+        temperature_of_fusion=0.01, smoothing_radius=0.05)
+    
+    
+    
+    progress = fenics.Progress('Time-stepping')
+
+    fenics.set_log_level(fenics.PROGRESS)
+    
+    with fenics.XDMFFile('output/stefan_problem/solution.xdmf') as solution_file:
+    
+        # Write the initial values
+        write_solution(solution_file, model.w_n, model.time)
+        
+        
+        # Solve a sequence of time steps.
+        end_time = 0.01
+        
+        while model.time < (end_time - fenics.DOLFIN_EPS):
+        
+            model.solve(adaptive=True, adaptive_tolerance=1.e-8)
 
             write_solution(solution_file, model.w_k, model.time)
             
@@ -380,11 +485,13 @@ def test_adaptive_melting_pcm():
     
 if __name__=='__main__':
     
+    test_1d_stefan_problem()
+    
     #test_adaptive_natural_convection_in_differentially_heated_cavity()
     
     #test_lid_driven_cavity()
     
     #test_adaptive_lid_driven_cavity()
     
-    test_adaptive_melting_pcm()
+    #test_adaptive_melting_pcm()
     
