bdd07168ea5557d08dbb339c34a58832a7ea61e0
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2017-12-06 22:57:55 +0100

    In work: Updated variable viscosity test. Getting nan's

--- tests/test_variable_viscosity.py
+++ tests/test_variable_viscosity.py
@@ -4,25 +4,27 @@ from .context import phaseflow
 import fenics
 
 
+Re = 100.
+
 def verify_against_ghia1982(w, mesh):
 
-    data = {'Re': 100, 'x': 0.5,
-        'y': [1.0000, 0.9766, 0.9688, 0.9609, 0.9531, 0.8516, 0.7344, 0.6172, 0.5000, 0.4531, 0.2813, 
+    data = {"Re": 100, "x": 0.5,
+        "y": [1.0000, 0.9766, 0.9688, 0.9609, 0.9531, 0.8516, 0.7344, 0.6172, 0.5000, 0.4531, 0.2813, 
               0.1719, 0.1016, 0.0703, 0.0625, 0.0547, 0.0000],
-        'ux': [1.0000, 0.8412, 0.7887, 0.7372, 0.6872, 0.2315, 0.0033, -0.1364, -0.2058, -0.2109, -0.1566, 
+        "ux": [1.0000, 0.8412, 0.7887, 0.7372, 0.6872, 0.2315, 0.0033, -0.1364, -0.2058, -0.2109, -0.1566, 
                -0.1015, -0.0643, -0.0478, -0.0419, -0.0372, 0.0000]}
     
     bbt = mesh.bounding_box_tree()
     
-    for i, true_ux in enumerate(data['ux']):
+    for i, true_ux in enumerate(data["ux"]):
     
-        p = fenics.Point(data['x'], data['y'][i])
+        p = fenics.Point(data["x"], data["y"][i])
         
         if bbt.collides_entity(p):
         
             wval = w(p)
             
-            ux = wval[0]
+            ux = Re*wval[0]
             
             assert(abs(ux - true_ux) < 2.e-2)
             
@@ -33,13 +35,13 @@ def test_variable_viscosity__nightly():
 
     ymin = -0.25
     
-    fixed_walls = "near(x[0],  0.) | near(x[0],  1.) | near(x[1],  '+str(ymin)+')"
+    fixed_walls = "near(x[0],  0.) | near(x[0],  1.) | near(x[1],  " + str(ymin) +")"
 
     left_middle = "near(x[0], 0.) && near(x[1], 0.5)"
     
     output_dir = "output/test_variable_viscosity"
     
-    mesh = fenics.RectangleMesh(fenics.Point(0., ymin), fenics.Point(1., 1.), 20, 25, 'crossed')
+    mesh = fenics.RectangleMesh(fenics.Point(0., ymin), fenics.Point(1., 1.), 20, 25, "crossed")
     
     
     # Refine the initial PCI.
@@ -72,27 +74,29 @@ def test_variable_viscosity__nightly():
         time_step_size = 1.,
         stop_when_steady = True,
         steady_relative_tolerance = 1.e-4,
-        thermal_conductivity = 0.,
-        liquid_viscosity = 0.01,
-        solid_viscosity = 1.e6,
-        temperature_of_fusion = -0.01,
+        liquid_thermal_conductivity = 0.,
+        solid_thermal_conductivity = 0.,
+        regularization_central_temperature = -0.01,
         regularization_smoothing_factor = 0.01,
         gravity = (0., 0.),
         stefan_number = 1.e16,
         adaptive = True,
         output_dir = output_dir,
-        initial_values_expression = (lid, "0.", "0.", "1. - 2.*(x[1] <= 0.)"),
+        initial_values_expression = (str(Re) + "*" + lid, "0.", "0.", "1. - 2.*(x[1] <= 0.)"),
         boundary_conditions = [
-            {'subspace': 0, 'value_expression': ("1.", "0."), 'degree': 3, 'location_expression': lid, 'method': 'topological'},
-            {'subspace': 0, 'value_expression': ("0.", "0."), 'degree': 3, 'location_expression': fixed_walls, 'method': 'topological'},
-            {'subspace': 1, 'value_expression': "0.", 'degree': 2, 'location_expression': left_middle, 'method': 'pointwise'}])
+            {"subspace": 0, "value_expression": (str(Re), "0."), "degree": 3, 
+                "location_expression": lid, "method": "topological"},
+            {"subspace": 0, "value_expression": ("0.", "0."), "degree": 3, 
+                "location_expression": fixed_walls, "method": "topological"},
+            {"subspace": 1, "value_expression": "0.", "degree": 2, 
+                "location_expression": left_middle, "method": "pointwise"}])
     
     
     # Verify against the known solution.
     verify_against_ghia1982(w, mesh)
 
     
-if __name__=='__main__':
+if __name__=="__main__":
 
     test_variable_viscosity__nightly()
     
